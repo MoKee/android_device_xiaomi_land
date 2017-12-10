59a8cc6a9d9d50d79bbddcc6828733e343f9e368
martincz <martincz@mokeedev.com>
2017-12-10 19:38:55 +0800

    land: mokee-ify
    Signed-off-by: IsaacChen <tingyi364@gmail.com>

--- device.mk
+++ device.mk
@@ -19,7 +19,7 @@ DEVICE_PATH := $(LOCAL_PATH)
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-mk
 
 # Include device-specific product fragments
 include $(LOCAL_PATH)/product/*.mk
---
+++ product/mokee.mk
@@ -0,0 +1,3 @@
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.mk.maintainer=IsaacChen
+ 
